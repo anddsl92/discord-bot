1326a4361374b2d7e2f6dd660b9caf44f3c1d168
anddsl92 <anddsl@hotmail.com>
2025-10-03 20:21:18 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -1326,7 +1326,7 @@ async def server_info_command(interaction: discord.Interaction):
 
 if __name__ == "__main__":
     # Lấy token từ biến môi trường (Render)
-    token = "MTQyMjU4OTI0NzExNzg1Njk3Mg.GA2zQj.SkfVAGpzfmyZ1K8I8txpf3sTbRST97Zv-BjcwU"
+    token = os.getenv('DISCORD_TOKEN')
     
     if not token:
         print("❌ Không tìm thấy DISCORD_TOKEN!")
